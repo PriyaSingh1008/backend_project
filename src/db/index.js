4fb43f514283de839210e9f58ef9187bc619488f
Priya Singh <singhpriya081003@gmail.com>
2024-04-11 00:18:22 +0530

    .env.sample

---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants";
+import connectDB from "./db/index.js";
+const coonectDB=async()=>{
+    try{
+      const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+      console.log(`\n MongoDB connected`);
+    }catch(error){
+        console.log("MONGODB connection error",error);
+        process.exit(1)
+    }
+} 
